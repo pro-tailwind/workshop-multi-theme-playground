da45d4d8888f6383bba641017dfa9974a1dac72d
Simon Vrachliotis <simon@simonswiss.com>
2022-07-01 09:01:13 +1000

    New theme file starting point

---
+++ workshop/themes.js
@@ -0,0 +1,29 @@
+const colors = require('tailwindcss/colors')
+
+const themes = {
+  default: {
+    50: colors.indigo[50],
+    100: colors.indigo[100],
+    200: colors.indigo[200],
+    300: colors.indigo[300],
+    400: colors.indigo[400],
+    500: colors.indigo[500],
+    600: colors.indigo[600],
+    700: colors.indigo[700],
+    800: colors.indigo[800],
+    900: colors.indigo[900],
+  },
+  // Let's create new themes with custom color shades
+  'ocean-green': {
+    50: '',
+    100: '',
+    200: '',
+    300: '',
+    400: '',
+    500: '',
+    600: '',
+    700: '',
+    800: '',
+    900: '',
+  },
+}
